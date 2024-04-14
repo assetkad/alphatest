06d24bd16f25415af71c83d490db0ee5fa955501
Asset Kad <asset_kad@mail.ru>
2024-04-15 02:44:21 +0500

    sidenav1 ui done

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -2,12 +2,42 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './ui/sidebar/sidebar.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
-  imports: [MatSidenavModule, SidebarComponent, RouterOutlet],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SidebarComponent,
+    RouterOutlet,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class LayoutComponent {}
+export default class LayoutComponent {
+  options: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.options = fb.group({
+      bottom: 0,
+      fixed: false,
+      top: 0,
+    });
+  }
+}
--- src/app/layout/ui/sidebar/sidebar.component.ts
+++ src/app/layout/ui/sidebar/sidebar.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatSidenavModule, MatButtonModule, MatIconModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidebarComponent {
+  @Output() toggleEvent = new EventEmitter();
 
+  toggle() {
+    this.toggleEvent.emit();
+  }
 }
