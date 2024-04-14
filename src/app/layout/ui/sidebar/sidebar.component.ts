662861ead4c4bc055058b6ab74c18f93f0f1e0f7
Asset Kad <asset_kad@mail.ru>
2024-04-15 03:16:04 +0500

    little changes

--- src/app/layout/ui/sidebar/sidebar.component.ts
+++ src/app/layout/ui/sidebar/sidebar.component.ts
@@ -7,15 +7,35 @@ import {
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import {
+  MAT_TOOLTIP_DEFAULT_OPTIONS,
+  MatTooltipDefaultOptions,
+  MatTooltipModule,
+} from '@angular/material/tooltip';
 import { CommonModule } from '@angular/common';
+
+export const myCustomTooltipDefaults: MatTooltipDefaultOptions = {
+  showDelay: 10,
+  hideDelay: 50000000,
+  touchendHideDelay: 1000,
+};
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    { provide: MAT_TOOLTIP_DEFAULT_OPTIONS, useValue: myCustomTooltipDefaults },
+  ],
 })
 export class SidebarComponent {
   @Output() toggleEvent = new EventEmitter();
